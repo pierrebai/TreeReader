75625944f3b34c955ab4b9efbee0bd278a83d25a
Pierre Baillargeon <pierrebai@hotmail.com>
2020-02-28 15:37:10 -0500

    Added icons.

--- TreeFilterApp/resource.h
+++ TreeFilterApp/resource.h
@@ -4,39 +4,15 @@
 //
 #define IDI_ICON1                       101
 #define IDI_APP_ICON                    101
-#define IDB_CANVAS_ROTATE               116
-#define IDB_CANVAS_TRANSLATE            117
-#define IDB_CANVAS_ZOOM                 118
-#define IDB_LAYER_ADD                   119
-#define IDB_LAYER_COPY                  120
-#define IDB_LAYER_DELETE                121
-#define IDB_LAYER_MOVE_DOWN             122
-#define IDB_LAYER_MOVE_UP               123
-#define IDB_MOSAIC_NEXT                 124
-#define IDB_MOSAIC_OPEN                 125
-#define IDB_MOSAIC_PREVIOUS             126
-#define IDB_MOSAIC_SAVE                 127
-#define IDB_EXPORT_IMG                  128
-#define IDB_EXPORT_SVG                  129
-#define IDB_BITMAP1                     134
-#define IDB_CANVAS_REDRAW               134
-#define IDB_REDO                        135
-#define IDB_BITMAP3                     136
-#define IDB_UNDO                        136
-#define IDB_TILING_EDITOR               137
-#define IDB_POLYGON_ADD                 138
-#define IDB_POLYGON_COPY                139
-#define IDB_POLYGON_DELETE              140
-#define IDB_POLYGON_DRAW                141
-#define IDB_POLYGON_INCLUDE             142
-#define IDB_POLYGON_MOVE                143
-#define IDB_POLYGON_CLEAR_FILL          144
-#define IDB_POLYGON_EXCLUDE_ALL         145
-#define IDB_POLYGON_FILL                146
-#define IDB_POLYGON_TRANSLATION         147
-#define IDB_BITMAP4                     148
-#define IDB_POLYGON_TRANS_CLEAR         148
-#define IDB_TILING_NEW                  149
+#define IDB_FILTER_ADD                   119
+#define IDB_FILTER_COPY                  120
+#define IDB_FILTER_DELETE                121
+#define IDB_FILTER_MOVE_DOWN             122
+#define IDB_FILTER_MOVE_UP               123
+#define IDB_TREE_OPEN                 124
+#define IDB_TREE_SAVE                 125
+#define IDB_REDO                        126
+#define IDB_UNDO                        127
 
 // Next default values for new objects
 // 
